58c89bf9983843e1f95c2a587feca0c1e557f955
Tiago Gomes <tidgomes@gmail.com>
2023-05-25 11:33:00 -0300

    render User in UsersList and pass name and age as props

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,5 +1,10 @@
-function User() {
-  return <li></li>
+function User({ name, age }) {
+  return (
+    <li>
+      <h1>{name}</h1>
+      <p>{age}</p>
+    </li>
+  )
 }
 
 export default User
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,5 +1,13 @@
-function UsersList() {
-  return <ul></ul>
+import User from './User'
+
+function UsersList({ users }) {
+  return (
+    <ul>
+      {users.map((user, index) => (
+        <User key={index} name={user.name} age={user.age} />
+      ))}
+    </ul>
+  )
 }
 
 export default UsersList
