f86a3b8827bf124973b2b1c6b9fab96af0948d74
Tiago Gomes <tidgomes@gmail.com>
2023-05-25 14:00:57 -0300

    add condition to render UsersList component

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -2,13 +2,13 @@ import User from './User'
 import classes from './UsersList.module.css'
 
 function UsersList({ users }) {
-  return (
+  return users.length ? (
     <ul className={classes.ul}>
       {users.map((user, index) => (
         <User key={index} name={user.name} age={user.age} />
       ))}
     </ul>
-  )
+  ) : null
 }
 
 export default UsersList
