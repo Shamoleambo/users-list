578565facaa41e0cf112da0846def0b422fbf896
Tiago Gomes <tidgomes@gmail.com>
2023-05-25 13:47:48 -0300

    adjust styling for the root div

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,8 +1,9 @@
 import User from './User'
+import classes from './UsersList.module.css'
 
 function UsersList({ users }) {
   return (
-    <ul>
+    <ul className={classes.ul}>
       {users.map((user, index) => (
         <User key={index} name={user.name} age={user.age} />
       ))}
